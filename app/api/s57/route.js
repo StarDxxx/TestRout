303c3d2ac12e8f38146dd3412fdbb83c25d67ab0
StarDxxx <g1831521035@163.com>
2024-09-25 14:54:39 +0800

    修正title s

--- app/api/s57/route.js
+++ app/api/s57/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const feed = new Feed({
-    title: "每日天气预报s34",
+    title: "每日天气预报s57",
     description: "feedId:61028264807309312+userId:55059127996282880",
     id: "http://example.com/",
     link: "http://example.com/",
